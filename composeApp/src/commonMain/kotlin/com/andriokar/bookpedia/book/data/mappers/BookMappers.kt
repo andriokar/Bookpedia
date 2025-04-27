8e9c3921d42610cd05aba23d1aac0d16306db35b
Andriy <karpus.andrii@proton.me>
2025-04-27 13:23:00 +0200

    Create book entity and mapper

--- composeApp/src/commonMain/kotlin/com/andriokar/bookpedia/book/data/mappers/BookMappers.kt
+++ composeApp/src/commonMain/kotlin/com/andriokar/bookpedia/book/data/mappers/BookMappers.kt
@@ -1,5 +1,6 @@
 package com.andriokar.bookpedia.book.data.mappers
 
+import com.andriokar.bookpedia.book.data.database.BookEntity
 import com.andriokar.bookpedia.book.data.dto.SearchedBookDto
 import com.andriokar.bookpedia.book.domain.Book
 
@@ -7,7 +8,7 @@ fun SearchedBookDto.toBook(): Book {
     return Book(
         id = id.substringAfterLast("/"),
         title = title,
-        imageUrl = if(coverKey != null) {
+        imageUrl = if (coverKey != null) {
             "https://covers.openlibrary.org/b/olid/${coverKey}-L.jpg"
         } else {
             "https://covers.openlibrary.org/b/id/${coverAlternativeKey}-L.jpg"
@@ -20,5 +21,21 @@ fun SearchedBookDto.toBook(): Book {
         ratingCount = ratingsCount,
         numPages = numPagesMedian,
         numEditions = numEditions ?: 0
+    )
+}
+
+fun Book.toBookEntity(): BookEntity {
+    return BookEntity(
+        id = id,
+        title = title,
+        imageUrl = imageUrl,
+        authors = authors,
+        description = description,
+        languages = languages,
+        firstPublishYear = firstPublishYear.toString(),
+        ratingsAverage = averageRating,
+        ratingsCount = ratingCount,
+        numPagesMedian = numPages,
+        numEditions = numEditions
     )
 }
